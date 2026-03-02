7f798f91cbf33a050069a2a3c7e01f0f36ee0aea
Abirami <110535911+Abirami1021@users.noreply.github.com>
2026-03-02 17:47:20 +0530

    Add files via upload

---
+++ JenkinMavenTest/src/test/java/mavjenkinpac/SampleTest.java
@@ -0,0 +1,21 @@
+package mavjenkinpac;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+
+    @Test
+    public void openGoogle() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
